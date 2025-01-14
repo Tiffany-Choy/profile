b85388f762667c11455ca9622b9686b56f74d4af
Tiffany <tiffanyachoy@gmail.com>
2025-01-14 13:50:11 -0800

    trial

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/" className="link-to-home">
+      <Link to="/profile" className="link-to-home">
         Tiffany Choy
       </Link>
       <ul>
