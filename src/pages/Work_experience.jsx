daabe6feb0c9087ffbac6af505f7a98abd9d7577
Tiffany <tiffanyachoy@gmail.com>
2025-07-18 18:00:39 -0700

    fixed images

--- src/pages/Work_experience.jsx
+++ src/pages/Work_experience.jsx
@@ -2,10 +2,9 @@ import Work from "../functions/Work";
 import "./stylesheets/Work_experience.css";
 import WorkData from "../data/work.json";
 export default function Work_experience() {
-  const base = import.meta.env.BASE_URL;  // "/" in dev, "/profile/" in prod
   return (
     <>
-    <h1>Work Experience</h1>
+      <h1>Work Experience</h1>
       {WorkData &&
         WorkData.map((job) => {
           return (
@@ -13,7 +12,7 @@ export default function Work_experience() {
               id={job.id}
               title={job.title}
               time={job.time}
-              logo={`${base}${job.logo.replace(/^\//, '')}`}
+              logo={job.logo}
               description={job.description}
               skills={job.skills}
             />
