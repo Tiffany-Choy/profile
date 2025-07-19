832b8a0f3d7c8ee3a5c096e046586a7988c04ef4
Tiffany <tiffanyachoy@gmail.com>
2025-07-18 17:50:47 -0700

    fixing images

--- src/pages/Work_experience.jsx
+++ src/pages/Work_experience.jsx
@@ -2,6 +2,7 @@ import Work from "../functions/Work";
 import "./stylesheets/Work_experience.css";
 import WorkData from "../data/work.json";
 export default function Work_experience() {
+  const base = import.meta.env.BASE_URL;  // "/" in dev, "/profile/" in prod
   return (
     <>
     <h1>Work Experience</h1>
@@ -12,7 +13,7 @@ export default function Work_experience() {
               id={job.id}
               title={job.title}
               time={job.time}
-              logo={job.logo}
+              logo={`${base}${job.logo.replace(/^\//, '')}`}
               description={job.description}
               skills={job.skills}
             />
